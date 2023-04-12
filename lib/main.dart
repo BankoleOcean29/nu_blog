c88cf47b584988234cf8fa79c611865d5733b298
BankoleOcean29 <bankolescripted@gmail.com>
2023-04-12 16:56:54 +0100

    mobile update

--- lib/main.dart
+++ lib/main.dart
@@ -14,24 +14,34 @@ class MyApp extends StatelessWidget {
       title: 'Singing Orange',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Delicious Handrawn'),
-      home: Blog(),
+      home: Blog(myName: '', myEmail: ''),
     );
   }
 }
 
 class Blog extends StatefulWidget {
-  const Blog({Key? key}) : super(key: key);
+  const Blog({
+    Key? key,
+    required this.myName,
+    required this.myEmail,
+  }) : super(key: key);
+
+  final String myName;
+  final String myEmail;
 
   @override
   State<Blog> createState() => _BlogState();
 }
 
 class _BlogState extends State<Blog> {
+  bool isHover = false;
+  bool isHover2 = false;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.orange,
+        backgroundColor: const Color(0xff8E5572),
         body: SingleChildScrollView(
           child: LayoutBuilder(
               builder: (BuildContext context, BoxConstraints constraints) {
@@ -39,58 +49,122 @@ class _BlogState extends State<Blog> {
               return Column(
                 children: [
                   const SizedBox(
-                    height: 30,
+                    height: 50,
+                  ),
+                  const Center(
+                    child: Text(
+                      'Silicon effect',
+                      style: TextStyle(fontSize: 30, color: Color(0xffBBBE64)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    height: 5,
+                    width: double.infinity,
+                    color: const Color(0xff0B0033),
+                  ),
+                  const SizedBox(
+                    height: 20,
                   ),
                   Row(
-                    children: const [
-                      SizedBox(
-                        width: 80,
+                    children: [
+                      const SizedBox(
+                        width: 10,
                       ),
-                      Text(
-                        'The Singing Orange',
-                        style: TextStyle(fontSize: 30),
+                      MouseRegion(
+                        onEnter: (PointerEvent event) {
+                          setState(() {
+                            isHover = true;
+                          });
+                        },
+                        onExit: (PointerEvent event) {
+                          setState(() {
+                            isHover = false;
+                          });
+                        },
+                        child: Text('hosted by Bankole Babarinsa.',
+                            style: TextStyle(
+                                color: isHover
+                                    ? Color(0xffBBBE64)
+                                    : Color(
+                                  0xff000000,
+                                ),
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400)),
                       ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+
                     ],
                   ),
                   const SizedBox(
-                    height: 20,
+                    height:20,
                   ),
-                  Container(
-                    color: Colors.black,
-                    height: 2,
-                    width: double.infinity,
+                  Text(
+                    'Bankolescripted@gmail.com',
+                    style: TextStyle(
+                        color: isHover2
+                            ? Color(0xffBBBE64)
+                            : Color(
+                          0xff000000,
+                        ),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400),
                   ),
                   const SizedBox(
-                    height: 20,
+                    height:20,
                   ),
-                  const Text(
-                    'hosted by Bankole Babarinsa',
-                    style: TextStyle(color: Colors.deepPurple),
-                  ),
-                  const SizedBox(
-                    height: 90,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 30.0),
-                    child: Align(
-                        alignment: Alignment.topLeft,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Container(
+                      height: 100,
+                      width: 1000,
+                      color: const Color(0xffBCAA99),
+                      child: const Center(
                         child: Text(
-                          'title: To err is Divine',
-                          style: TextStyle(fontSize: 20),
-                        )),
+                          'Understanding mobile applications',
+                          style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.w800,
+                              color: Color(0xff443850)),
+                        ),
+                      ),
+                    ),
                   ),
                   const SizedBox(
-                    height: 10,
+                    height: 40,
                   ),
-                  Container(
-                    color: const Color(0xff8D6E63),
-                    //height: 900,
-                    width: 350,
-                    child: const Text(
-                      'She sings like lust, and dances like love, or is it the other '
-                      'way around?, I\'m not sure, but she had me, from the very'
-                      'first moment. Jupiter and Venus would delight',
-                      style: TextStyle(fontSize: 25, height: 1.5),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Container(
+                      height: 100,
+                      width: 1000,
+                      color: const Color(0xffBCAA99),
+                      child: const Center(
+                        child: Text(
+                          'How could you build an MVP',
+                          style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.w800,
+                              color: Color(0xff443850)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 90.0, left: 220),
+                    child: SizedBox(
+                      height: 70,
+                      width: 70,
+                      child: FloatingActionButton(onPressed: () {},
+                      backgroundColor: const Color(0xffBBBE64),
+                      child: Icon(Icons.mail),),
                     ),
                   )
                 ],
@@ -103,8 +177,8 @@ class _BlogState extends State<Blog> {
                   ),
                   const Center(
                     child: Text(
-                      'Singing Orange',
-                      style: TextStyle(fontSize: 100),
+                      'Silicon effect',
+                      style: TextStyle(fontSize: 100, color: Color(0xffBBBE64)),
                     ),
                   ),
                   const SizedBox(
@@ -113,23 +187,102 @@ class _BlogState extends State<Blog> {
                   Container(
                     height: 5,
                     width: double.infinity,
-                    color: Color(0xff0B0033),
+                    color: const Color(0xff0B0033),
                   ),
                   const SizedBox(
                     height: 50,
                   ),
                   Row(
-                    children: const [
-                      SizedBox(
+                    children: [
+                      const SizedBox(
                         width: 100,
                       ),
-                      Text(
-                        'hosted by Bankole Ocean',
-                        style: TextStyle(color: Color(0xff832232),
-                        fontSize: 20),
-                      )
+                      MouseRegion(
+                        onEnter: (PointerEvent event) {
+                          setState(() {
+                            isHover = true;
+                          });
+                        },
+                        onExit: (PointerEvent event) {
+                          setState(() {
+                            isHover = false;
+                          });
+                        },
+                        child: Text('hosted by Bankole Babarinsa.',
+                            style: TextStyle(
+                                color: isHover
+                                    ? Color(0xffBBBE64)
+                                    : Color(
+                                        0xff000000,
+                                      ),
+                                fontSize: 40,
+                                fontWeight: FontWeight.w700)),
+                      ),
+                      const SizedBox(
+                        width: 600,
+                      ),
+                      MouseRegion(
+                        onEnter: (PointerEvent event) {
+                          setState(() {
+                            isHover2 = true;
+                          });
+                        },
+                        onExit: (PointerEvent event) {
+                          setState(() {
+                            isHover2 = false;
+                          });
+                        },
+                        child: Text(
+                          'Bankolescripted@gmail.com',
+                          style: TextStyle(
+                              color: isHover2
+                                  ? Color(0xffBBBE64)
+                                  : Color(
+                                      0xff000000,
+                                    ),
+                              fontSize: 40,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
                     ],
-                  )
+                  ),
+                  const SizedBox(
+                    height: 250,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 1000,
+                    color: const Color(0xffBCAA99),
+                    child: const Center(
+                      child: Text(
+                        'Understanding mobile applications',
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.w800,
+                            color: Color(0xff443850)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 140,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 1000,
+                    color: const Color(0xffBCAA99),
+                    child: const Center(
+                      child: Text(
+                        'How could you build an MVP',
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.w800,
+                            color: Color(0xff443850)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 140,
+                  ),
                 ],
               );
             }
